565ff10c13d9d964f55d624ea665ad5d810580bb
高庆明 <gaoqingming@xueleyun.com>
2019-02-15 15:42:25 +0800

    encode注释

--- api/src/main/java/info/gaofei/rpc/serialize/RpcEncoder.java
+++ api/src/main/java/info/gaofei/rpc/serialize/RpcEncoder.java
@@ -18,6 +18,8 @@ public class RpcEncoder extends MessageToByteEncoder {
     public void encode(ChannelHandlerContext ctx, Object in, ByteBuf out) throws Exception {
         if (genericClass.isInstance(in)) {
             byte[] data = SerializationUtil.serialize(in);
+            //写入本次数据长度，获取的时候前四位就是数据的长度，并根据数据长度判断是否接收到了所有数据，（nio有可能受到部分数据）
+            //可以去瞅瞅Decoder
             out.writeInt(data.length);
             out.writeBytes(data);
         }
